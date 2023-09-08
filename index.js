2cd12382e037f8c7c299bdab660ab5a0e146bc42
Nicholas Lee <nicholasleexyz@gmail.com>
2023-09-07 18:05:28 -0600

    progress on getting data

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ const PORT = 8000
 const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
+const pretty = require('pretty')
 
 const app = express()
 
-
-const url = "https://worldofsucculents.com/succupedia/"
+const _URL_ = "https://worldofsucculents.com/succupedia/"
 
 app.get('/', (req, res) => {
   res.json('Welcome to my succulent information API. ^_____^')
 })
 
 app.get('/succulents', (req, res) => {
-  const plants = []
+  const basePlantData = []
 
-  axios.get(url)
+  axios.get(_URL_)
     .then((response) => {
       const html = response.data
       const $ = cheerio.load(html)
@@ -26,11 +26,35 @@ app.get('/succulents', (req, res) => {
         const title = elem.text()
         const url = elem.attr('href')
         // console.log("URL: " + url);
-        plants.push({title, url})
+        basePlantData.push({title, url})
       })
-      // console.log(...plants)
-      res.json(plants);
 
+      // console.log(...plants)
+      // const temp = [basePlantData[0]]
+
+      basePlantData.forEach((data) => {
+        const u = data.url;
+        // console.log("url: " + u);
+        const plants = []
+        axios.get(u).then((r) => {
+          const _html = r.data
+          const $ = cheerio.load(_html);
+          const headers = $('div.post-content.cf.entry-content.content-normal > h4')
+          const pData = []
+          headers.each(function() {
+            const title = $(this)
+            // console.log("Elem: "+ elem);
+
+            const data = {title: title.text(), paragraph: title.next().text()} 
+            plants.push(data)
+          })
+          console.log(plants)
+        })
+
+      });
+
+
+      res.json(basePlantData);
     }).catch((err) => console.log(err))
 })
 
